083edb230fc6ff0559b7463980f163bffba23d76
Sridhar Ratnakumar <srid@srid.ca>
2020-03-19 17:15:10 -0400

    Add ChangeLog

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Change Log for srid.ca
+
+Updates to srid.ca will be made here. You may "watch" the Github repository using "Releases only" to get notified.
+
+## Unreleased
+
+## 2020-03-19
+
+- Rewrote srid.ca based on [neuron](https://neuron.srid.ca/), my Zettelkasten tool.
+- Published [announcement post](https://www.srid.ca/2010101.html), as well as, [documentation guide](https://neuron.srid.ca/) for neuron.
--- default.nix
+++ default.nix
@@ -1,8 +1,8 @@
 let 
   # The commit hash of http://github.com/srid/neuron to use
-  neuronRev = "53cbd619d13699335e2011f928265d8d172c6048";
+  neuronRev = "e518174";
 in {
-  neuron ? ../neuron # builtins.fetchTarball "https://github.com/srid/neuron/archive/${neuronRev}.tar.gz"
+  neuron ? builtins.fetchTarball "https://github.com/srid/neuron/archive/${neuronRev}.tar.gz"
 # Cabal project root
 , root ? ./.
 # Cabal project name
